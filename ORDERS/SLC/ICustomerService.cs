c22102174ed61894f507447104d2b0ba5986dd7f
MaGs-Zz <miguelgomezan439@gmail.com>
2024-07-28 02:32:48 -0500

    feat(back-end): Add layer SLC, Interface ICustomerService.cs to SLC

---
+++ ORDERS/SLC/ICustomerService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SLC
+{
+    public interface ICustomerService
+
+    {
+
+        Task<ActionResult<Customer>> CreateAsync([FromBody] Customer toCreate);
+
+        Task<IActionResult> DeleteAsync(int id);
+
+
+        Task<ActionResult<List<Customer>>> GetAll();
+
+
+        Task<ActionResult<Customer>> RetrieveAsync(int id);
+
+
+        Task<IActionResult> UpdateAsync(int id, [FromBody] Customer toUpdate);
+
+    }
+}

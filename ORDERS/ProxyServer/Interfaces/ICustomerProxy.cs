f5ee8df96d7af0cc924204b94b952b9bbade2c4e
MaGs-Zz <miguelgomezan439@gmail.com>
2024-07-30 15:51:10 -0500

    feat(back-end): Add proyect ProxyServer, Class CustomerProxy with his methods and interface ICustomerProxy

---
+++ ORDERS/ProxyServer/Interfaces/ICustomerProxy.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyServer.Interfases
+{
+    public interface ICustomerProxy
+    {
+        Task<Customer> CreateAsync(Customer customer);
+
+        Task<bool> DeleteAsync(int id);
+
+        Task<List<Customer>> GetAllAsync();
+
+        Task<Customer> GetByIdAsync(int id);
+
+        Task<bool> UpdateAsync(int id, Customer customer);
+
+    }
+}
